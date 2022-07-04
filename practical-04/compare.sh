e1027c52d68f11984aac9c7c5dad2d6cdf07b389
fhildeb <felix@lukso.io>
2022-07-04 18:45:59 +0200

    add comarison and sorts

---
+++ practical-04/compare.sh
@@ -0,0 +1,3 @@
+# cmp file1 file 2                  Bytewise comparison
+# diff file1 file2                  Rowwise comparison
+# comm -1,2 file1 file2             Rowwise comparison, ignoring row 1 and 2
---
+++ practical-04/sort.sh
@@ -0,0 +1,13 @@
+# Transform text or input
+# sort                              sort alphabetically
+# sort -k 2,4                       sort field 2 to 3
+# sort -k 2,2                       sort in field 2
+# sort -n                           sort numerically
+# sort -k 3,3 -n                    sort numerically in field 3
+# sort -k 3,3 -m -u                 sort numerbically in field 3 and delete duplicate entries
+
+`ls -al | sort -k 5 -n`             # Pipe to sort field 5 numerically ascending
+# sort -t:                          Colon will be seen as separator for calumns
+# sort -t" "                        Space will be seen as separator for calumns
+
+# sort -o output-file               Write sort output into output-file
