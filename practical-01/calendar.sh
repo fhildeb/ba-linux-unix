be6d69e3d8bee052a938084b5d164d87d150c779
fhildeb <felix@lukso.io>
2022-07-04 18:41:59 +0200

    add calendar and commands

---
+++ practical-01/calendar.sh
@@ -0,0 +1,4 @@
+echo "Current calendar month: "     `cal`
+echo "Calendar of year 2019: "      `cal 2019`
+echo "Calendar of october 2019: "   `cal 10 2019`
+echo "Current date and time: "      `date`
---
+++ practical-01/commands.sh
@@ -0,0 +1,6 @@
+
+echo "My login name: "              `whoami`
+echo "All logged-in users: "        `who`
+echo "User and group ID's: "        `id`
+echo "Current path: "               `pwd`
+echo "Home Directory: "             $HOME
