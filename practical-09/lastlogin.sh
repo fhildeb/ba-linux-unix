f70b8ba93733b13348d2b73c53774a9f53ca26ed
fhildeb <felix@lukso.io>
2022-07-04 18:57:05 +0200

    add last login

---
+++ practical-09/lastlogin.sh
@@ -0,0 +1,17 @@
+if [ $# -eq 1 ]; then
+	if [ "$1" = "-w" ]; then
+		`date >> ".lastlog"`
+		echo `tail -1 .lastlog`
+	else
+		echo "Leider die unpassenden Argumente!"
+	fi
+elif [ $# -eq 0 ]; then
+	if [ -e ".lastlog" ]; then
+		echo `tail -1 .lastlog`
+	else
+		touch ".lastlog"
+	fi
+else
+	echo "Leider die unpassenden Argumente!"
+fi
+exit 0
