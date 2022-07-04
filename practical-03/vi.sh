2b8b75d43f91de4fdbd68c60591bec231854a475
fhildeb <felix@lukso.io>
2022-07-04 18:45:20 +0200

    add vi commands

---
+++ practical-03/vi.sh
@@ -0,0 +1,27 @@
+# Create or open file
+`vi testfile.md`
+
+# Command inputs (esc)
+# :wq               write and quit
+# :q                quit without making changes
+# :q!               discard and quit
+# :w                save
+# :/ old            search for "old"
+# :1,19s/old/new    search for "old" from line 1 to 19 and replace it with "new"
+# :set nu           enable row numeration
+# :set nonu         disable row numeration
+
+# Integrated terminal
+# :!sh              open command line
+# exit              come back to VI                
+
+# Command mode normal inputs (esc)
+# x                 remove char
+# dd                remove line
+# J                 join rows
+# u                 reverse last action
+# U                 restore current row
+
+# Input mode (i)
+# Enter             line break
+
