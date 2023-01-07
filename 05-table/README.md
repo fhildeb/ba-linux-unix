195041b75578526bba0b8f65f548c017fce7e16e
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-01-07 14:36:30 +0100

    table bugfix and file output overhaul

--- 05-table/README.md
+++ 05-table/README.md
@@ -9,36 +9,85 @@ Cheatcheats for content sorting from file tables:
 
 ## Output
 
-### Table
+### Complex Task
 
 ```bash
 Number of lines of the file friends:
 4
 
 File content sorted by name alphabetically:
-ABC         def     01234   Pilzstadt   Pilzweg         1
-CGI         web     00224   Obstdorf    Birnental       4
-Dieter  Hans    78902   ABC-Hain        Birkenallee     67
-Google  chief   12345   GoogleTown      First Drive     1
-Loew    Dieter  09648   Mittweida       Technikumplatz  14
+ABC         def     01234   Pilzstadt       Pilzlobweg          1
+CGI         web     00224   Obstdorf        Birnental           4
+Dieter      Hans    78902   ABC-Hain        Birkenallee         67
+Google      chief   12345   GoogleTown      Low Drive           1
+Loew        Dieter  09648   Mittweida       Technikumplatz      14
 
 File content sorted by postal code numerically:
-CGI         web     00224   Obstdorf    Birnental       4
-ABC         def     01234   Pilzstadt   Pilzweg         1
-Loew    Dieter  09648   Mittweida       Technikumplatz  14
-Google  chief   12345   GoogleTown      First Drive     1
-Dieter  Hans    78902   ABC-Hain        Birkenallee     67
+CGI         web     00224   Obstdorf        Birnental           4
+ABC         def     01234   Pilzstadt       Pilzlobweg          1
+Loew        Dieter  09648   Mittweida       Technikumplatz      14
+Google      chief   12345   GoogleTown      Low Drive           1
+Dieter      Hans    78902   ABC-Hain        Birkenallee         67
 
 File content by name, but only display of the first two fields:
 ABC         def
 CGI         web
-Dieter  Hans
-Google  chief
-Loew    Dieter
+Dieter      Hans
+Google      chief
+Loew        Dieter
 
 Sorted content of the fields for postal code and location into postal_code.
 
 Search file friends for entries for the name Meier:
-Loew    Dieter  09648   Mittweida       Technikumplatz  14
-Dieter  Hans    78902   ABC-Hain        Birkenallee     67
+Loew    Dieter  09648   Mittweida   Technikumplatz  14
+Dieter  Hans    78902   ABC-Hain    Birkenallee     67
+```
+
+### Task
+
+```bash
+Sorting by alphabetical order, result output to friend_location.
+
+Alphabetical sort without upper/lowercase distinction to friend_location_2.
+
+Sort by alphabet order with respect to the location:
+Dieter      Hans    78902   ABC-Hain    Birkenallee     67
+Google      chief   12345   GoogleTown  Low Drive       1
+Loew        Dieter  09648   Mittweida   Technikumplatz  14
+CGI         web     00224   Obstdorf    Birnental       4
+ABC         def     01234   Pilzstadt   Pilzlobweg      1
+
+Replacing all lowercase with uppercase letters and save to friends_upper.
+
+Removing all TAB characters from friends and save to friends_wo_tab.
+
+How many entries contain the name Dieter?
+2
+
+How many entries contain the name Lo regardless of upper/lowercase?
+3
+
+All complete lines containing the name Dieter
+Loew    Dieter  09648   Mittweida   Technikumplatz  14
+Dieter  Hans    78902   ABC-Hain    Birkenallee     67
+
+Search of sys in two files: friends and friend_location:
+friends:            Loew    Dieter  09648   Mittweida   Technikumplatz  14
+friends:            Google  chief   12345   GoogleTown  Low Drive       1
+friend_location:    Google  chief   12345   GoogleTown  Low Drive       1
+friend_location:    Loew    Dieter  09648   Mittweida   Technikumplatz  14
+
+All regular files with owner rights rwx:
+-rwxr-xr-x  1 fhildeb  staff   222  7 Jan 14:24 friends
+
+Files which have read permission for everyone:
+-rw-r--r--  1 fhildeb  staff  1421  7 Jan 14:25 README.md
+-rw-r--r--  1 fhildeb  staff   606  7 Jan 13:22 complex_task.sh
+-rw-r--r--  1 fhildeb  staff   223  7 Jan 14:33 friend_location
+-rw-r--r--  1 fhildeb  staff   223  7 Jan 14:33 friend_location_2
+-rwxr-xr-x  1 fhildeb  staff   222  7 Jan 14:24 friends
+-rw-r--r--  1 fhildeb  staff   222  7 Jan 14:33 friends_upper
+-rw-r--r--  1 fhildeb  staff   198  7 Jan 14:33 friends_wo_tab
+-rw-r--r--  1 fhildeb  staff    97  7 Jan 13:22 postal_code
+-rw-r--r--  1 fhildeb  staff  2062  7 Jan 14:33 tasks.sh
 ```
--- 05-table/tasks.sh
+++ 05-table/tasks.sh
@@ -1,61 +1,61 @@
 # Sorting
 
-# a) Sorting by alphabetical order (name), result output to the file friend location
-`sort freunde > freundesort`
+echo "Sorting by alphabetical order, result output to friend_location."
+sort friends > friend_location
+echo -e ""
+echo "Alphabetical sort without upper/lowercase distinction to friend_location_2."
+sort -f friends > friend_location_2
+echo -e ""
+echo "Sort by alphabet order with respect to the location:"
+sort -k4 friends
+echo -e ""
+echo "Replacing all lowercase with uppercase letters and save to friends_upper."
+cat friends | tr '[:lower:]' '[:upper:]' > friends_upper
+echo -e ""
+echo "Removing all TAB characters from friends and save to friends_wo_tab."
+cat friends | tr -d '\t' > friends_wo_tab
+echo -e ""
 
-# b) Like a), but without distinction of upper/lower case letters during sorting
-`sort -f freunde > freundesort`
-
-# c) Sort by alphabet. Order with respect to the place! (i.e. all fields before the Location field are not taken into account for sorting!)
-`sort -k 4 freunde`
-
-# d) Replace all lowercase letters of the file friends with uppercase letters, save the result in the file friendslarge
-`tr '[:lower:]' '[:upper:]' freunde > freundegross`
-
-# e) Remove all TAB characters from the file friends, result to the file freunde_ohne_tab.
-`tr -d '\t' freunde > freunde_ohne_tab`
-
-# f) What does tr -cs '[a-z][A-Z]' '[\012*]' friends do?
+# What does tr -cs '[a-z][A-Z]' '[\012*]' friends do?
 # Replaces all characters that are not letters with enter. 
 # Important: "*" sees sequence number otherwise 2x enter if number equals 15
 
-# g) How many entries (lines) in your file contain the name "Meier"?
-`grep -c Meier freunde`
+echo "How many entries contain the name Dieter?"
+grep -c Dieter friends
+echo -e ""
+echo "How many entries contain the name Lo regardless of upper/lowercase?"
+grep -ci lo friends
+echo -e ""
+echo -e "All complete lines containing the name Dieter"
+grep Dieter friends
+echo -e ""
+echo -e "Search of sys in two files: friends and friend_location:"
+grep ch friends friend_location
+echo -e ""
 
-# h) Like g), but ignore upper/lower case (e.g. "Meier" or "meier" permissible)
-`grep -ci Meier freunde`
-
-# i) List the complete line(s) containing the name "Meier".
-`grep Meier freunde`
-
-# j) List the line(s) containing the string "sys", but this time the command should search in two files, friends and /etc/passwd.
-`grep sys freunde /etc/password`
-
-# k) grep '^U..' file
 # Search all with beginning U and two following characters
+# grep '^U..' file
 
-# l) grep '^[A-Z]' file
 # Search all with capital letters at the beginning
+# grep '^[A-Z]' file
 
-# m) grep '^[A-Z].*' file
 # Search all with capital letters and at least one suffix
+# grep '^[A-Z].*' file
 
-# n) grep '^[A-Z].*\.c$' file
 # Search all with uppercase letters at the beginning and at least one suffix and .c at the end
+# grep '^[A-Z].*\.c$' file
 
-# o) grep '^[0-9A-Fa-f][0-9A-Fa-f]*$' file
 # Search everything with a hexadecimal number at the beginning and at least one hexadecimal number at the end
+# grep '^[0-9A-Fa-f][0-9A-Fa-f]*$' file
 
-# p) List all lines in which the last name starts with S and contains at least 3 characters
-`grep '^\t S..' freunde`
+# List all lines in which the last name starts with S and contains at least 3 characters
+# grep '^\t S..' file
 
-# q) List all lines in which the string 09648 occurs anywhere
-`grep 09648 reunde`
+# List all lines in which the string 09648 occurs anywhere
+# grep 09648 reunde
 
-# r) Form a pipe from the ls command (with option -l) and grep to determine from the contents of the current 
-# directory all entries that belong to a simple (regular) file for which the owner has the rights rwx! For this, 
-# remember the exact structure of the display for ls -l
-`ls -l | grep '-rwx......'`
-
-# s) like r) but for simple files for which the rest of the world has read permission!
-`ls -l | grep '-...r..r..'`
+echo "All regular files with owner rights rwx:"
+ls -l | grep '^\-rwx'
+echo -e ""
+echo "Files which have read permission for everyone:"
+ls -l | grep '\-...r..r..'
