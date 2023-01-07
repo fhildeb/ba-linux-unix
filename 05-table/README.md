10218d63a4bfa29e8a7093fda9b130f6461acf08
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-01-07 13:23:09 +0100

    renames and update commands of table session

--- 05-table/README.md
+++ 05-table/README.md
@@ -6,3 +6,39 @@ Cheatcheats for content sorting from file tables:
 
 - Generic shell commands in `tasks.sh`
 - Linked shell commands in `compex_tasks.sh`
+
+## Output
+
+### Table
+
+```bash
+Number of lines of the file friends:
+4
+
+File content sorted by name alphabetically:
+ABC         def     01234   Pilzstadt   Pilzweg         1
+CGI         web     00224   Obstdorf    Birnental       4
+Dieter  Hans    78902   ABC-Hain        Birkenallee     67
+Google  chief   12345   GoogleTown      First Drive     1
+Loew    Dieter  09648   Mittweida       Technikumplatz  14
+
+File content sorted by postal code numerically:
+CGI         web     00224   Obstdorf    Birnental       4
+ABC         def     01234   Pilzstadt   Pilzweg         1
+Loew    Dieter  09648   Mittweida       Technikumplatz  14
+Google  chief   12345   GoogleTown      First Drive     1
+Dieter  Hans    78902   ABC-Hain        Birkenallee     67
+
+File content by name, but only display of the first two fields:
+ABC         def
+CGI         web
+Dieter  Hans
+Google  chief
+Loew    Dieter
+
+Sorted content of the fields for postal code and location into postal_code.
+
+Search file friends for entries for the name Meier:
+Loew    Dieter  09648   Mittweida       Technikumplatz  14
+Dieter  Hans    78902   ABC-Hain        Birkenallee     67
+```
--- 05-table/complex_task.sh
+++ 05-table/complex_task.sh
@@ -1,20 +1,17 @@
-# 1) Display of the number of lines of the file freunde
-`wc -l freunde`
-
-# 2) Display of the file content sorted by name (alphabetically)
-`sort freunde`
-
-# 3) Display of the file content sorted by postal code (numerically)
-`sort -k3 -n freunde`
-
-# 4) Sorting of the file content by name, but only display of the first two fields (= columns name and first name).
-`sort freunde | cut -f1,2`
-
-# 5) Sorting of the file content by location, transfer of the sorted content of the fields for postal code and location into the file postlz.
-`sort -3k freunde | cut -f3,4 > postlz`
-
-# 6 ) Display of the file postlz,where in the display all lower case letters are to be replaced by upper case letters
-`tr '[a-z]' '[A-Z]' < freunde > postlz`
-
-# 7) Search in the file freunde for entries for the name Meier.
-`grep Meier freunde`
+echo "Number of lines of the file friends:"
+wc -l friends | awk '{$1=$1};1' | tr -d 'friends'
+echo -e ""
+echo "File content sorted by name alphabetically:"
+sort friends
+echo -e ""
+echo "File content sorted by postal code numerically:"
+sort -k3 -n friends
+echo -e ""
+echo "File content by name, but only display of the first two fields:"
+sort friends | cut -f1,2
+echo -e ""
+echo "Sorted content of the fields for postal code and location into postal_code."
+sort -k3 friends | cut -f3,4 | awk '{$1=$1};1' > postal_code
+echo -e ""
+echo "Search file friends for entries for the name Meier:"
+grep Dieter friends
